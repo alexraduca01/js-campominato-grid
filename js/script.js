b4c16f16cc6f09f4d64291fbe1a7c04b86d9aae2
alexraduca01 <alex.raduca01@icloud.com>
2023-10-18 15:04:58 +0200

    added squareGen function

--- js/script.js
+++ js/script.js
@@ -11,3 +11,27 @@
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+function squareGen() {
+
+    wrapper.classList.remove('d-none');
+    btn.classList.add('d-none');
+
+    const squareNumber = 100;
+
+    for(let a = 1; a <= squareNumber; a++){
+        const square = document.createElement('div');
+        square.classList.add('square', 'd-flex', 'justify-content-center', 'align-items-center');
+        wrapper.append(square);
+        square.innerHTML = (a);
+        square.addEventListener('click', function(){
+            square.classList.add('bg-sky', 'text-white');
+        })
+    }
+    
+}
+
+const btn = document.querySelector('button');
+const wrapper = document.getElementById('wrapper');
+
+btn.addEventListener('click', squareGen)
